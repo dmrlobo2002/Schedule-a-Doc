6f1b2e61c8dce70bf83c32bd7472babaa97f92dc
dmrlobo2002 <64146581+dmrlobo2002@users.noreply.github.com>
2023-02-05 14:05:59 -0500

    Updated User model

--- server/models/user.go
+++ server/models/user.go
@@ -4,8 +4,10 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Order struct {
+type User struct {
 	ID         	primitive.ObjectID 	`bson:"_id"`
 	Username       	*string            	`json:"username"`
 	Password		*string				`json:"password"`
+	FirstName		*string				`json:"firstName"`
+	LastName		*string				`json:"lastName"`
 }
