35027795c0649cce9afbcbcaedc07d0662e8c253
DivyanshiSaini <sainid@ufl.edu>
2023-02-05 22:51:05 -0500

    SignIn update

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -4,8 +4,9 @@ import './Login.css';
 export const Login = () => {
   return(
     <div className="Login">
-      <div className="Box">
-        <div className="Signin"> Sign in</div>  
+      <div className="outerBox">
+        <div className="innerBox">
+         <div className="Signin"> Sign in</div>  
         <div className="inputs">
           <input className="email" placeholder="Email or Phone"/>
             <input className="password" placeholder="Password" />
@@ -19,7 +20,8 @@ export const Login = () => {
             </div> 
             <div className="FP" style={{display:"flex", justifyContent: "center"}}>
             <button className="ForgotPassword"> Forgot Password? </button>
-            </div>
+            </div>  
+        </div>
       </div>
     </div>
   );
