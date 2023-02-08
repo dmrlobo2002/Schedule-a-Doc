4ee3af6c92ee65b6b5e9f6d31c8ea06e4d719d7d
DivyanshiSaini <sainid@ufl.edu>
2023-02-08 08:05:46 -0500

    Made code format consistent

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -4,25 +4,25 @@ import './Login.css';
 
 export const Login = () => {
   return(
-    <div className="Login">
+    <div className="signin">
       <div className="outerBox bg-gradient-to-r from-cyan-500 to-blue-500 shadow-2xl">
         <div className="innerBox">
-         <div className="Signin"> Sign in</div>  
+         <div className="signinText"> Sign in</div>  
         <div className="inputs">
-          <input className="InEmail pl-3" placeholder="Email or Phone"/>
-            <input type={"password"} className="InPassword pl-3" placeholder="Password" />
+          <input className="inEmail" placeholder="Email or Phone"/>
+            <input type={"password"} className="inPassword" placeholder="Password" />
         </div> 
         <div style={{display:"flex", justifyContent: "center"}}>
            <button className="loginButton">LOGIN</button>         
           </div>  
-          <div className="SignupMessage" style={{display:"flex", justifyContent: "center"}}>
+          <div className="signupMessage" style={{display:"flex", justifyContent: "center"}}>
             Don't have an account? 
             <Link to={"/signup"}>
-            <button className="SignupButton"> SIGN UP</button>
+            <button className="signupOption"> SIGN UP</button>
             </Link>
             </div> 
             <div className="FP" style={{display:"flex", justifyContent: "center"}}>
-            <button className="ForgotPassword"> Forgot Password? </button>
+            <button className="forgotPassword"> Forgot Password? </button>
             </div>  
         </div>
       </div>
