6d249ace9a212c93d1af51620721f28217898dfd
dmrlobo2002 <64146581+dmrlobo2002@users.noreply.github.com>
2023-02-11 17:51:33 -0500

    Commented out sample routes

--- server/main.go
+++ server/main.go
@@ -27,14 +27,10 @@ func main() {
 	router.POST("/signup", routes.CreateUser)
 	router.POST("/appointment", routes.CreateAppointment)
 	//R
-	// router.GET("/waiter/:waiter", routes.GetOrdersByWaiter)
-	// router.GET("/orders", routes.GetOrders)
-	// router.GET("/order/:id/", routes.GetOrderById)
+	
 	//U
-	// router.PUT("/waiter/update/:id", routes.UpdateWaiter)
-	// router.PUT("/order/update/:id", routes.UpdateOrder)
+	
 	//D
-	// router.DELETE("/order/delete/:id", routes.DeleteOrder)
 
 	//this runs the server and allows it to listen to requests.
 	router.Run(":" + port)
