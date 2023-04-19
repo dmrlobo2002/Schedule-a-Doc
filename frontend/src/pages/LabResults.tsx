1c54906196d02a0f839bfe6de42acb4faf71cda4
RAMERCADO1 <andremercado345@gmail.com>
2023-04-19 19:03:40 -0400

    Doctor Profile

--- frontend/src/pages/LabResults.tsx
+++ frontend/src/pages/LabResults.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import queryString from 'query-string';
+//import queryString from 'query-string';
 import axios from "axios";
 import SideBarD from "../components/sidebar/SideBardD";
 import "./LabResults.css"
