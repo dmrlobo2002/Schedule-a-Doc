c9754f11a8a826a7554487e6e8a42f1b0d567de4
Krepel <francokrepel@ufl.edu>
2023-04-19 13:42:41 -0400

    little stuff, working on frontend and created sprint 4

--- frontend/src/pages/PatientScheduling.tsx
+++ frontend/src/pages/PatientScheduling.tsx
@@ -147,10 +147,10 @@ const PatientScheduling = () => {
     return doctorOptions;
   };
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 py-6 sm:py-12">
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col items-center justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
         <div className="absolute inset-0 bg-gradient-to-r from-green-400 to-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 mx-auto">
           <h1 className="text-4xl font-semibold mb-6">Schedule an Appointment</h1>
           <form onSubmit={handleSubmit}>
             <div className="mb-4">
@@ -197,7 +197,7 @@ const PatientScheduling = () => {
         </div>
       </div>
     </div>
-  );
+  );  
 };
 
 // export default PatientScheduling;
