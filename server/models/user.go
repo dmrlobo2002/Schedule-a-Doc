4889d5ab3ba600af974479f5238adb8ff9b505e0
Lobo <daniel.lobo@ufl.edu>
2023-04-19 14:41:05 -0400

    new tests

--- server/models/user.go
+++ server/models/user.go
@@ -6,10 +6,11 @@ import (
 
 type User struct {
 	ID          primitive.ObjectID `bson:"_id"`
-	Email       *string            `json:"email"`
-	PhoneNumber *string            `json:"phoneNumber"`
-	Password    *string            `json:"password"`
-	FirstName   *string            `json:"firstName"`
-	LastName    *string            `json:"lastName"`
+	Email       *string            `json:"email" binding:"required,email"`
+	PhoneNumber *string            `json:"phoneNumber" binding:"required"`
+	Password    *string            `json:"password" binding:"required"`
+	FirstName   *string            `json:"firstName" binding:"required"`
+	LastName    *string            `json:"lastName" binding:"required"`
 	IsDoctor    *bool              `json:"isDoctor"`
 }
+
