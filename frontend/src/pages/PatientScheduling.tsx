4623cb94cedb11b2d0e7b1234dd32b15f1f0dbf8
Krepel <francokrepel@ufl.edu>
2023-04-19 14:52:46 -0400

    centered the Appointment Scheduling Card

--- frontend/src/pages/PatientScheduling.tsx
+++ frontend/src/pages/PatientScheduling.tsx
@@ -147,7 +147,7 @@ const PatientScheduling = () => {
     return doctorOptions;
   };
   return (
-    <div className="min-h-screen bg-gray-100 py-6 flex flex-col items-center justify-center sm:py-12">
+    <div className=" w-full fixed top-20 py-6 flex flex-col items-center justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
         <div className="absolute inset-0 bg-gradient-to-r from-green-400 to-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 mx-auto">
